4a9012cc6ebe66910e6e6f9875771104b8dff614
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-16 06:48:43 +0800

    feat: Add event listener for fetching weather data when the user searched a location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+import getWeatherData from './getWeatherData';
+
+// Get Weather data when the user searched for a location.
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+    // Prevent form submission.
+    event.preventDefault();
+
+    // Get the location.
+    const search = form.querySelector('#search-location');
+    const location = search.value;
+
+    // Get Weather data.
+    const weatherData = getWeatherData(location);
+});
