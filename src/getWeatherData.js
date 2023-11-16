4b39fbd81794eeff582dcf4f0cc58671bd08c514
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-17 06:45:10 +0800

    feat: Add initial features for fetching weather data

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -1,1 +1,14 @@
-export default function getWeatherData(location) {}
+export default async function getWeatherData(location) {
+    // Fetch wheather data.
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=0f21e3cdb5ce4371bf4215345230411&q=${location}`, {
+            mode: 'cors',
+        });
+        const json = await response.json();
+
+        return json;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
