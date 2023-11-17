25e9a15c8d075952d82ddf6278c3e2a947ef9696
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-17 10:10:50 +0800

    feat: Use forecast request to also get other days' weather prediction

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -1,7 +1,7 @@
 export default async function getWeatherData(location) {
     // Fetch wheather data.
     try {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=0f21e3cdb5ce4371bf4215345230411&q=${location}`, {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=0f21e3cdb5ce4371bf4215345230411&q=${location}&days=10`, {
             mode: 'cors',
         });
         const json = await response.json();
