795d7b25befae3892b103c712951dad8bf58dc9e
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-18 19:10:58 +0800

    feat: Access the n-day-forecast-container in DOM

--- src/buildNdayForecastDashboard.js
+++ src/buildNdayForecastDashboard.js
@@ -1,1 +1,4 @@
-export default function buildNdayForecastDashBoard(daywiseHourlyForecast) {}
+export default function buildNdayForecastDashBoard(daywiseHourlyForecast) {
+    // Access the n-day-forecast-container in DOM.
+    const nDayForecastContainer = document.querySelector('.n-day-forecast-container');
+}
