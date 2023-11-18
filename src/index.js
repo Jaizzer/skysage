ca76c75882912b1e238e89e943ab8793eadeba63
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-18 08:18:46 +0800

    feat: Implement async function to await data from getWeatherData

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.css';
 import getWeatherData from './getWeatherData';
 
+async function loadWeatherStatus(location) {
+    // Get Weather data.
+    const weatherData = await getWeatherData(location);
+}
+
 // Get Weather data when the user searched for a location.
 const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
@@ -11,6 +16,5 @@ form.addEventListener('submit', (event) => {
     const search = form.querySelector('#search-location');
     const location = search.value;
 
-    // Get Weather data.
-    const tenDayForecast = getWeatherData(location);
+    loadWeatherStatus(location);
 });
