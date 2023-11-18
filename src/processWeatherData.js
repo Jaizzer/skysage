25ae0b35effd8c9d0743919b429449b443eb111a
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-18 12:07:49 +0800

    doc: Document the processWeatherData module

--- src/processWeatherData.js
+++ src/processWeatherData.js
@@ -1,8 +1,20 @@
+// Import the Weather class from the Weather module.
 import Weather from './Weather';
 
+/**
+ * Processes the raw weather data obtained from the WeatherAPI and extracts day-wise hourly forecasts.
+ *
+ * @param {Object} weatherData - The raw weather data obtained from the WeatherAPI.
+ * @returns {Array} An array containing day-wise hourly forecasts represented by Weather objects.
+ * @throws Will throw an error if the structure of the WeatherAPI response is not as expected.
+ */
 export default function processWeatherData(weatherData) {
+    // Destructure relevant data from the weatherData object.
     const { forecastLocation, tenDayForecast } = weatherData;
+
+    // Map over each day in the ten-day forecast.
     const daywiseHourlyForecasts = tenDayForecast.map((day) => {
+        // Map over each hour in a day and create a new Weather object for each hour.
         return day.hour.map((hour) => {
             return new Weather(
                 forecastLocation.name,
@@ -25,5 +37,6 @@ export default function processWeatherData(weatherData) {
         });
     });
 
+    // Return the array of day-wise hourly forecasts.
     return daywiseHourlyForecasts;
 }
