03d674661b56e48652b0b2e1d2ff00a21609ad04
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-18 12:08:13 +0800

    doc: Document the Weather object

--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,24 @@
+/**
+ * Represents weather information for a specific location at a particular moment.
+ *
+ * @class Weather
+ * @param {string} location - The name of the location.
+ * @param {number} highTemperature - The high temperature for the day (in degrees Celsius).
+ * @param {number} lowTemperature - The low temperature for the day (in degrees Celsius).
+ * @param {string} generalWeatherStatus - The general weather condition for the day.
+ * @param {number} currentTemperature - The current temperature (in degrees Celsius).
+ * @param {string} currentHourWeatherStatus - The weather condition for the current hour.
+ * @param {number} uvIndex - The UV index.
+ * @param {string} sunset - The time of sunset.
+ * @param {string} sunrise - The time of sunrise.
+ * @param {number} wind - The wind speed (in miles per hour).
+ * @param {number} precipitation - The amount of precipitation (in millimeters).
+ * @param {number} feelsLike - The "feels like" temperature (in degrees Celsius).
+ * @param {number} humidity - The humidity percentage.
+ * @param {number} visibility - The visibility distance (in kilometers).
+ * @param {number} pressure - The atmospheric pressure (in millibars).
+ * @param {number} averages - The average temperature for the day (in degrees Celsius).
+ */
 export default class Weather {
     constructor(
         location,
