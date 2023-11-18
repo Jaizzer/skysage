4a2a1d7ea2c7be9a647e8c46f9ba1b97f92c7a29
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-18 12:09:03 +0800

    doc: Document getCurrentHour module

--- src/getCurrentHour.js
+++ src/getCurrentHour.js
@@ -1,3 +1,9 @@
+/**
+ * Retrieves the current hour of the day.
+ *
+ * @function getCurrentHour
+ * @returns {number} The current hour (in 24-hour format).
+ */
 export default function getCurrentHour() {
     // Get the current date and extract the hour component.
     const currentHour = new Date().getHours();
