44a1a02f3c5cf33c4e140e8016f787908717e5f2
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-22 23:39:05 +0800

    feat: Add utility function that returns a message base on the uv value

--- src/buildUvIndexCard.js
+++ src/buildUvIndexCard.js
@@ -1,1 +1,18 @@
-export default function buildUvIndexCard(currentHourWeatherStatus) {}
+export default function buildUvIndexCard(uvValue) {}
+
+function getUVMessage(uvValue) {
+    console.log(uvValue);
+    if (uvValue >= 0 && uvValue <= 2) {
+        return 'Minimal danger. Up to 1 hour sun, no burning.';
+    } else if (uvValue >= 3 && uvValue <= 5) {
+        return 'Low risk. Fair skin burns in <20 mins. Use sunscreen, wear protection.';
+    } else if (uvValue >= 6 && uvValue <= 7) {
+        return 'Moderate risk. Fair skin burns in <20 mins. Use sunscreen, protect sensitive areas.';
+    } else if (uvValue >= 8 && uvValue <= 10) {
+        return 'High risk. Fair skin burns in <10 mins. Minimize exposure, use sunscreen, wear protection.';
+    } else if (uvValue >= 11) {
+        return 'Very high risk. Fair skin burns in <5 mins. Minimize exposure, use high SPF sunscreen, wear protection.';
+    } else {
+        return 'Invalid UV value. Provide a valid UV index.';
+    }
+}
