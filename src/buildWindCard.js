5cf35a0eca52ae80394ce883887466c96d4b52cf
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-11-30 07:59:35 +0800

    feat: Add module that builds the current wind status card

---
+++ src/buildWindCard.js
@@ -0,0 +1,65 @@
+export default function buildWindStatusCard(currentWindStatus) {
+    // Create the div element
+    const windStatusDiv = document.createElement('div');
+    windStatusDiv.classList.add('highlight-card');
+
+    // Create the div for the Wind Status name
+    const nameDiv = document.createElement('div');
+    nameDiv.classList.add('highlight-name');
+    nameDiv.textContent = 'Wind';
+    windStatusDiv.appendChild(nameDiv);
+
+    // Create the div for the Wind Speed value
+    const speedDiv = document.createElement('div');
+    speedDiv.classList.add('value');
+    speedDiv.textContent = `${currentWindStatus.windSpeed} km/h`;
+    windStatusDiv.appendChild(speedDiv);
+
+    // Create the div for the Wind Direction value
+    const directionDiv = document.createElement('div');
+    directionDiv.classList.add('value');
+    directionDiv.textContent = `${currentWindStatus.windDirection}`;
+    windStatusDiv.appendChild(directionDiv);
+
+    // Create the div for additional notes
+    const notesDiv = document.createElement('div');
+    notesDiv.classList.add('additional-notes');
+    notesDiv.textContent = getWindMessage(currentWindStatus.windSpeed); // Use the wind message as additional notes
+    windStatusDiv.appendChild(notesDiv);
+
+    return windStatusDiv;
+}
+
+// Example function to get wind message based on wind speed
+// Utility function to get wind message based on wind speed using Beaufort scale
+function getWindMessage(windSpeed) {
+    let message = '';
+
+    if (windSpeed < 1) {
+        message = 'Calm conditions.';
+    } else if (windSpeed < 4) {
+        message = 'Light air.';
+    } else if (windSpeed < 7) {
+        message = 'Gentle breeze.';
+    } else if (windSpeed < 11) {
+        message = 'Moderate wind.';
+    } else if (windSpeed < 17) {
+        message = 'Fresh breeze.';
+    } else if (windSpeed < 22) {
+        message = 'Strong wind.';
+    } else if (windSpeed < 28) {
+        message = 'High wind, moderate gale, near gale.';
+    } else if (windSpeed < 34) {
+        message = 'Gale.';
+    } else if (windSpeed < 41) {
+        message = 'Strong gale.';
+    } else if (windSpeed < 48) {
+        message = 'Storm.';
+    } else if (windSpeed < 56) {
+        message = 'Violent storm.';
+    } else {
+        message = 'Hurricane force.';
+    }
+
+    return message;
+}
