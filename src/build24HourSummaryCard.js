af5f524bb83c6043879199ee5d5be9b3156f57dd
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-04 08:59:06 +0800

    feat: Add utility funtion that extracts 24-hour summary starting from the current hour

--- src/build24HourSummaryCard.js
+++ src/build24HourSummaryCard.js
@@ -1,1 +1,14 @@
+import getCurrentHour from './getCurrentHour';
+
 export default function build24HourSummaryCard(daywiseHourlyForecasts) {}
+
+function get24HourSummary(daywiseHourlyForecasts) {
+    const currentHour = getCurrentHour();
+
+    let twentyFourHourSummary = [];
+
+    twentyFourHourSummary = twentyFourHourSummary.concat(daywiseHourlyForecasts[0].slice(currentHour));
+    twentyFourHourSummary = twentyFourHourSummary.concat(daywiseHourlyForecasts[1].slice(0, currentHour + 1));
+
+    return twentyFourHourSummary;
+}
