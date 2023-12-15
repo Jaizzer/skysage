6085305c39f5172bbe8d5a8347cf14b234ae7f33
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-16 06:30:03 +0800

    feat: Throw error if the location input is not found

--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -4,6 +4,12 @@ export default async function getWeatherData(location) {
         const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=0f21e3cdb5ce4371bf4215345230411&q=${location}&days=10`, {
             mode: 'cors',
         });
+
+        // Throw error if location is not found
+        if (response.status !== 200) {
+            throw new Error(`Failed to fetch data. Status: ${response.status}`);
+        }
+
         const json = await response.json();
         const extractedInfo = { forecastLocation: json.location, nDayForecast: json.forecast.forecastday };
         console.log(extractedInfo);
