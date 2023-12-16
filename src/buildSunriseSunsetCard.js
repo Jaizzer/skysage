d88b07c5e814e249e845844f4674109523343064
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-17 07:19:54 +0800

    feat: Add icons for the sunrise sunset card

--- src/buildSunriseSunsetCard.js
+++ src/buildSunriseSunsetCard.js
@@ -1,17 +1,36 @@
+import sunsetImgSrc from './img/sunset.png';
+import sunriseImgSrc from './img/sunrise.png';
+import sunImgSrc from './img/sun.png';
+
 export default function buildSunriseSunsetCard(sunrise, sunset) {
     // Create the div element
     const sunriseSunsetDiv = document.createElement('div');
-    sunriseSunsetDiv.classList.add('highlight-card');
+    sunriseSunsetDiv.classList.add('highlight-card', 'sunrise-sunset');
 
-    // Create the div for the sunset sunrise card name.
+    // Create the heading container.
+    const headingContainer = document.createElement('div');
+    headingContainer.classList.add('heading-container');
+    sunriseSunsetDiv.appendChild(headingContainer);
+
+    // Create the image element (assuming there's an icon URL you want to insert)
+    const iconContainer = document.createElement('div');
+    iconContainer.classList.add('icon-container');
+    headingContainer.appendChild(iconContainer);
+
+    const icon = document.createElement('img');
+    icon.classList.add('highlight-icon');
+    icon.src = sunImgSrc;
+    iconContainer.appendChild(icon);
+
+    // Create the div for the UV Index name
     const nameDiv = document.createElement('div');
     nameDiv.classList.add('highlight-name');
-    nameDiv.textContent = 'Sunrise & Sunset';
-    sunriseSunsetDiv.appendChild(nameDiv);
+    nameDiv.textContent = 'Sun';
+    headingContainer.appendChild(nameDiv);
 
     // Create the image element (assuming there's an icon URL you want to insert)
     const sunriseIconImg = document.createElement('img');
-    sunriseIconImg.src = '';
+    sunriseIconImg.src = sunriseImgSrc;
     sunriseIconImg.classList.add('icon');
     sunriseSunsetDiv.appendChild(sunriseIconImg);
 
@@ -23,8 +42,8 @@ export default function buildSunriseSunsetCard(sunrise, sunset) {
 
     // Create the image element (assuming there's an icon URL you want to insert)
     const sunsetIconImg = document.createElement('img');
-    sunsetIconImg.src = '';
-    sunriseIconImg.classList.add('icon');
+    sunsetIconImg.src = sunsetImgSrc;
+    sunsetIconImg.classList.add('icon');
     sunriseSunsetDiv.appendChild(sunsetIconImg);
 
     // Create div for the sunrise time.
