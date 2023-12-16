6975485eeb9cdc49485083b6d51d5daed06f56fc
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-17 07:47:16 +0800

    feat: Update values of moon card

--- src/buildMoonStatusCard.js
+++ src/buildMoonStatusCard.js
@@ -23,13 +23,13 @@ export default function buildMoonStatusCard(moonPhase, moonRise) {
     // Create the div for the UV Index name
     const nameDiv = document.createElement('div');
     nameDiv.classList.add('highlight-name');
-    nameDiv.textContent = moonPhase;
+    nameDiv.textContent = 'Moon';
     headingContainer.appendChild(nameDiv);
 
     // Create the div for the moon value (Moon rise time).
     const valueDiv = document.createElement('div');
     valueDiv.classList.add('value');
-    valueDiv.textContent = `Moonrise: ${moonRise}`;
+    valueDiv.textContent = `${moonPhase}, ${moonRise}`;
     moonStatusDiv.appendChild(valueDiv);
 
     return moonStatusDiv;
