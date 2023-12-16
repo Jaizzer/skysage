eca122e70726a037194c0ad3abca127987932d5a
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-17 07:36:28 +0800

    feat: Add icon to pressure card

--- src/buildPressureStatusCard.js
+++ src/buildPressureStatusCard.js
@@ -1,13 +1,30 @@
+import pressureImgSrc from './img/pressure.png';
+
 export default function buildPressureStatusCard(pressure) {
     // Create the div element
     const pressureStatusDiv = document.createElement('div');
     pressureStatusDiv.classList.add('highlight-card');
 
-    // Create the div for the pressure title.
+    // Create the heading container.
+    const headingContainer = document.createElement('div');
+    headingContainer.classList.add('heading-container');
+    pressureStatusDiv.appendChild(headingContainer);
+
+    // Create the image element (assuming there's an icon URL you want to insert)
+    const iconContainer = document.createElement('div');
+    iconContainer.classList.add('icon-container');
+    headingContainer.appendChild(iconContainer);
+
+    const icon = document.createElement('img');
+    icon.classList.add('highlight-icon');
+    icon.src = pressureImgSrc;
+    iconContainer.appendChild(icon);
+
+    // Create the div for the UV Index name
     const nameDiv = document.createElement('div');
     nameDiv.classList.add('highlight-name');
     nameDiv.textContent = 'Pressure';
-    pressureStatusDiv.appendChild(nameDiv);
+    headingContainer.appendChild(nameDiv);
 
     // Create the div for the pressure value
     const valueDiv = document.createElement('div');
