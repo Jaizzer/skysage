5a51ea8b57cfa8e3909dfaf3772a0c9e9e8917fd
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-17 07:40:56 +0800

    feat: Add icon for feels like card

--- src/buildFeelsLikeCard.js
+++ src/buildFeelsLikeCard.js
@@ -1,13 +1,29 @@
+import feelsLikeImgSrc from './img/feelsLike.png';
+
 export default function buildFeelsLikeCard(feelsLike) {
     // Create the div element
     const feelsLikeStatusDiv = document.createElement('div');
     feelsLikeStatusDiv.classList.add('highlight-card');
 
-    // Create the div for the feels-like title.
+    // Create the heading container.
+    const headingContainer = document.createElement('div');
+    headingContainer.classList.add('heading-container');
+    feelsLikeStatusDiv.appendChild(headingContainer);
+
+    // Create the image element (assuming there's an icon URL you want to insert)
+    const iconContainer = document.createElement('div');
+    iconContainer.classList.add('icon-container');
+    headingContainer.appendChild(iconContainer);
+
+    const icon = document.createElement('img');
+    icon.classList.add('highlight-icon');
+    icon.src = feelsLikeImgSrc;
+    iconContainer.appendChild(icon);
+
     const nameDiv = document.createElement('div');
     nameDiv.classList.add('highlight-name');
     nameDiv.textContent = 'Feels Like';
-    feelsLikeStatusDiv.appendChild(nameDiv);
+    headingContainer.appendChild(nameDiv);
 
     // Create the div for the feels-like value
     const valueDiv = document.createElement('div');
