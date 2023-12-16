643700ec3a2954ced67603f41d01c6c489235cbf
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-17 07:30:53 +0800

    feat: Add icon for visibility card

--- src/buildVisibilityCard.js
+++ src/buildVisibilityCard.js
@@ -1,13 +1,30 @@
+import visibilityImgSrc from './img/visibility.png';
+
 export default function buildVisibilityCard(visibility) {
     // Create the div element
     const visibilityStatusDiv = document.createElement('div');
     visibilityStatusDiv.classList.add('highlight-card');
 
-    // Create the div for the visibility title.
+    // Create the heading container.
+    const headingContainer = document.createElement('div');
+    headingContainer.classList.add('heading-container');
+    visibilityStatusDiv.appendChild(headingContainer);
+
+    // Create the image element (assuming there's an icon URL you want to insert)
+    const iconContainer = document.createElement('div');
+    iconContainer.classList.add('icon-container');
+    headingContainer.appendChild(iconContainer);
+
+    const icon = document.createElement('img');
+    icon.classList.add('highlight-icon');
+    icon.src = visibilityImgSrc;
+    iconContainer.appendChild(icon);
+
+    // Create the div for the UV Index name
     const nameDiv = document.createElement('div');
     nameDiv.classList.add('highlight-name');
     nameDiv.textContent = 'Visibility';
-    visibilityStatusDiv.appendChild(nameDiv);
+    headingContainer.appendChild(nameDiv);
 
     // Create the div for the visibility value
     const valueDiv = document.createElement('div');
