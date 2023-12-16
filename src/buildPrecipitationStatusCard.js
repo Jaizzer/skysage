47ccd7b346f0be89125fe90eab0c07d5d4cd15af
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-17 07:28:30 +0800

    feat: Add icon for precipitation card

--- src/buildPrecipitationStatusCard.js
+++ src/buildPrecipitationStatusCard.js
@@ -1,13 +1,30 @@
+import precipitationImgSrc from './img/precipitation.png';
+
 export default function buildPreciptationStatusCard(precipitation) {
     // Create the div element
     const precipitationStatusDiv = document.createElement('div');
     precipitationStatusDiv.classList.add('highlight-card');
 
-    // Create the div for the precipitation title.
+    // Create the heading container.
+    const headingContainer = document.createElement('div');
+    headingContainer.classList.add('heading-container');
+    precipitationStatusDiv.appendChild(headingContainer);
+
+    // Create the image element (assuming there's an icon URL you want to insert)
+    const iconContainer = document.createElement('div');
+    iconContainer.classList.add('icon-container');
+    headingContainer.appendChild(iconContainer);
+
+    const icon = document.createElement('img');
+    icon.classList.add('highlight-icon');
+    icon.src = precipitationImgSrc;
+    iconContainer.appendChild(icon);
+
+    // Create the div for the UV Index name
     const nameDiv = document.createElement('div');
     nameDiv.classList.add('highlight-name');
     nameDiv.textContent = 'Precipitation';
-    precipitationStatusDiv.appendChild(nameDiv);
+    headingContainer.appendChild(nameDiv);
 
     // Create the div for the precipitation value
     const valueDiv = document.createElement('div');
