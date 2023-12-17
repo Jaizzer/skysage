7243ac0d45a63b652693fd2e5c0e2b7aa07c0d0b
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-17 10:25:22 +0800

    feat: Add corresponding messages for precipitation data

--- src/buildPrecipitationStatusCard.js
+++ src/buildPrecipitationStatusCard.js
@@ -32,5 +32,39 @@ export default function buildPreciptationStatusCard(precipitation) {
     valueDiv.textContent = `${precipitation} mm`;
     precipitationStatusDiv.appendChild(valueDiv);
 
+    // Create the div for additional notes
+    const notesDiv = document.createElement('div');
+    notesDiv.classList.add('additional-notes');
+    notesDiv.textContent = getPrecipitationMessage(precipitation);
+    precipitationStatusDiv.appendChild(notesDiv);
+
     return precipitationStatusDiv;
+}
+
+// Return a correponding message base on the precipitation data.
+function getPrecipitationMessage(precipitation) {
+    let precipitationMessage;
+    switch (true) {
+        case precipitation === 0:
+            precipitationMessage = 'No rainfall';
+            break;
+        case precipitation > 0 && precipitation <= 5:
+            precipitationMessage = 'Light rain, carry an umbrella.';
+            break;
+        case precipitation > 5 && precipitation <= 15:
+            precipitationMessage = 'Moderate rain, expect wet conditions.';
+            break;
+        case precipitation > 15 && precipitation <= 30:
+            precipitationMessage = 'Heavy rain, caution in travel.';
+            break;
+        case precipitation > 30 && precipitation <= 50:
+            precipitationMessage = 'Very heavy rain, potential flooding.';
+            break;
+        case precipitation > 50 && precipitation <= 75:
+            precipitationMessage = 'Intense rain, high flood risk.';
+            break;
+        default:
+            precipitationMessage = 'Extreme rain, major flooding possible.';
+    }
+    return precipitationMessage;
 }
