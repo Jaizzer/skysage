914da8ebc2674069e65e73b7cd37987973610d2c
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-18 09:52:11 +0800

    feat: Add a function that takes a time in HH:MM AM/PM format and returns an object containg hours and minutes in 24-hour format

--- src/build24HourSummaryCard.js
+++ src/build24HourSummaryCard.js
@@ -63,3 +63,23 @@ function convertTo12HourFormat(hour) {
     const amPm = hour < 12 ? 'AM' : 'PM';
     return `${hours12} ${amPm}`;
 }
+
+function convertTo24HourFormat(hour) {
+    // Split HH:MM and AM/PM
+    let [hoursMinutes, amOrPm] = hour.split(' ');
+    // Split HH and MM.
+    let [hours, minutes] = hoursMinutes.split(':');
+
+    // Convert to integers.
+    hours = parseInt(hours);
+    minutes = parseInt(minutes);
+
+    if (amOrPm === 'PM') {
+        hours = hours + 12;
+    }
+
+    return {
+        hours: hours,
+        minutes: minutes,
+    };
+}
