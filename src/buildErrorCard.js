aff21b4e39e60fdbefef38a7903d58625d06df99
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-20 07:08:24 +0800

    doc: Add module documentation to buildErrorCard

--- src/buildErrorCard.js
+++ src/buildErrorCard.js
@@ -1,5 +1,27 @@
+// Import the errorImageSrc for the default error image.
 import errorImageSrc from './img/no-location.png';
 
+/**
+ * Builds an error card with a custom error message.
+ *
+ * @param {string} message - The error message to be displayed in the error card.
+ * @returns {HTMLElement} The error card background element, which can be appended to the DOM.
+ *
+ * This function creates an error card with the following components:
+ * - Blurry Background: Adds a semi-transparent background overlay to visually highlight the error card.
+ * - Error Card: The main container for the error message and associated elements.
+ * - Close Button: An 'X' button to close the error card.
+ * - Error Image: An image representing the default error state.
+ * - Error Message: The custom error message provided as a parameter.
+ *
+ * When the close button is clicked, the error card background is removed from the DOM, and the associated form is enabled and cleared.
+ *
+ * @example
+ * // Example usage:
+ * const errorMessage = "Location not found. Please try again.";
+ * const errorCardBackground = buildErrorCard(errorMessage);
+ * document.body.appendChild(errorCardBackground);
+ */
 export default function buildErrorCard(message) {
     // Create the error-card background.
     const errorCardBackground = document.createElement('div');
