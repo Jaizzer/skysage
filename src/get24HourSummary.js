7ee98409146ab7772503f8889b3c2ee28e5c6241
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-20 10:59:07 +0800

    doc: Update documentation of the get24HourSummar module

--- src/get24HourSummary.js
+++ src/get24HourSummary.js
@@ -3,35 +3,41 @@ import Sunrise from './Sunrise';
 import Sunset from './Sunset';
 import { convertTo24HourFormat } from './formatTime';
 
+/**
+ * Generates a 24-hour summary of hourly forecasts with special objects for sunrise and sunset events.
+ *
+ * @param {Array} daywiseHourlyForecasts - An array containing day-wise hourly forecasts represented by Weather objects.
+ * @returns {Array} A 24-hour summary of hourly forecasts with added Sunrise and Sunset objects.
+ *
+ * This function takes day-wise hourly forecasts and generates a 24-hour summary of hourly forecasts.
+ * It includes special objects representing Sunrise and Sunset events for today and tomorrow, depending on the current time.
+ * The returned array contains instances of Weather, Sunrise, and Sunset objects.
+ */
 export default function get24HourSummary(daywiseHourlyForecasts) {
+    // Get the current time in 24-hour format.
     const currentTime = convertTo24HourFormat(daywiseHourlyForecasts[0][0].date.timeToday);
 
+    // Initialize the 24-hour summary array.
     let twentyFourHourSummary = [];
 
+    // Concatenate today's and tomorrow's hourly forecasts based on the current time.
     twentyFourHourSummary = twentyFourHourSummary.concat(daywiseHourlyForecasts[0].slice(currentTime.hours));
     twentyFourHourSummary = twentyFourHourSummary.concat(daywiseHourlyForecasts[1].slice(0, currentTime.hours + 1));
 
-    // Get hours and minutes of today's sunrise time.
+    // Get hours and minutes of today's sunrise and sunset times.
     const todaySunriseTime = convertTo24HourFormat(daywiseHourlyForecasts[0][0].sunrise);
-
-    // Get hours and minutes of today's sunset time.
     const todaySunsetTime = convertTo24HourFormat(daywiseHourlyForecasts[0][0].sunset);
 
-    // Get hours and minutes of tomorrow's sunrise time.
+    // Get hours and minutes of tomorrow's sunrise and sunset times.
     const tomorrowSunriseTime = convertTo24HourFormat(daywiseHourlyForecasts[1][0].sunrise);
-
-    // Get hours and minutes of tomorrow's sunset time.
     const tomorrowSunsetTime = convertTo24HourFormat(daywiseHourlyForecasts[1][0].sunset);
 
-    // If today's sunrise is yet to happen, create an object.
+    // Create Sunrise object for today if today's sunrise is yet to happen.
     if (
         todaySunriseTime.hours > currentTime.hours ||
         (todaySunriseTime.hours === currentTime.hours && todaySunriseTime.minutes > currentTime.minutes)
     ) {
-        // daywiseHourlyForecasts[0][0] was arbitrarily chosen because daywiseHourlyForecasts[0][n] all have the same sunrise time.
         const todaySunrise = new Sunrise(daywiseHourlyForecasts[0][0].sunrise);
-
-        // Get the index of the hour-forecast that has the same hour with today's sunrise hour.
         const index = twentyFourHourSummary.findIndex((element) => {
             if (element instanceof Weather) {
                 return parseInt(element.date.dayOfInterestTime.split(':')[0]) === todaySunriseTime.hours;
@@ -39,11 +45,8 @@ export default function get24HourSummary(daywiseHourlyForecasts) {
         });
         twentyFourHourSummary.splice(index + 1, 0, todaySunrise);
     } else {
-        // Create tomorrow's sunrise if today's sunrise has already happened.
-        // daywiseHourlyForecasts[1][0] was arbitrarily chosen because daywiseHourlyForecasts[1][n] all have the same sunrise time.
+        // Create Sunrise object for tomorrow if today's sunrise has already happened.
         const tomorrowSunrise = new Sunrise(daywiseHourlyForecasts[1][0].sunrise);
-
-        // Get the index of the hour-forecast that has the same hour with today's sunrise hour but for tomorrow's date.
         const index = twentyFourHourSummary.findIndex((element) => {
             if (element instanceof Weather) {
                 return (
@@ -55,12 +58,9 @@ export default function get24HourSummary(daywiseHourlyForecasts) {
         twentyFourHourSummary.splice(index + 1, 0, tomorrowSunrise);
     }
 
-    // If today's sunset is yet to happen, create an object.
+    // Create Sunset object for today if today's sunset is yet to happen.
     if (todaySunsetTime.hours > currentTime.hours || (todaySunsetTime.hours === currentTime.hours && todaySunsetTime.minutes > currentTime.minutes)) {
-        // daywiseHourlyForecasts[0][0] was arbitrarily chosen because daywiseHourlyForecasts[0][n] all have the same sunset time.
         const todaySunset = new Sunset(daywiseHourlyForecasts[0][0].sunset);
-
-        // Get the index of the hour-forecast that has the same hour with today's Sunset hour.
         const index = twentyFourHourSummary.findIndex((element) => {
             if (element instanceof Weather) {
                 return parseInt(element.date.dayOfInterestTime.split(':')[0]) === todaySunsetTime.hours;
@@ -68,11 +68,8 @@ export default function get24HourSummary(daywiseHourlyForecasts) {
         });
         twentyFourHourSummary.splice(index + 1, 0, todaySunset);
     } else {
-        // Create tomorrow's sunset if today's sunset has already happened.
-        // daywiseHourlyForecasts[1][0] was arbitrarily chosen because daywiseHourlyForecasts[1][n] all have the same sunset time.
+        // Create Sunset object for tomorrow if today's sunset has already happened.
         const tomorrowSunset = new Sunset(daywiseHourlyForecasts[1][0].sunset);
-
-        // Get the index of the hour-forecast that has the same hour with today's Sunset hour but for tomorrow's date.
         const index = twentyFourHourSummary.findIndex((element) => {
             if (element instanceof Weather) {
                 return (
