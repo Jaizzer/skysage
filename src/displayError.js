8595a8d1248a374dc77b368c7f190ddcca1e3aa7
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-20 10:54:36 +0800

    doc: Add documentation to the displayError module

--- src/displayError.js
+++ src/displayError.js
@@ -1,10 +1,26 @@
 import buildErrorCard from './buildErrorCard';
 
+/**
+ * Displays an error message using a custom error card.
+ *
+ * @param {string} message - The error message to be displayed.
+ * @returns {HTMLElement} The generated error card background element.
+ *
+ * This function disables the search bar and creates a custom error card with the provided error message.
+ * The error card includes a close button ('X'), an error image, and the specified error message.
+ * The generated error card background is appended to the document body.
+ *
+ * To handle user interaction, the error card includes a close button that, when clicked,
+ * removes the error card background and re-enables the search bar.
+ */
 export default function displayError(message) {
-    // Remove focus from the search bar while error message is still unclosed.
+    // Remove focus from the search bar while the error message is still unclosed.
     const searchBar = document.querySelector('input');
     searchBar.disabled = true;
 
+    // Create the error card and append it to the document body.
     const errorCard = buildErrorCard(message);
     document.body.appendChild(errorCard);
+
+    return errorCard; // Return the error card background element for potential further manipulation.
 }
