0acee3045eaaa993e1ba7c91c652eac72c23dc39
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-20 10:55:22 +0800

    doc: Add documentation to the formatTime module

--- src/formatTime.js
+++ src/formatTime.js
@@ -1,9 +1,27 @@
+/**
+ * Converts a 24-hour format hour to a 12-hour format with AM/PM designation.
+ *
+ * @param {number} hour - The hour in 24-hour format (0 to 23).
+ * @returns {string} The hour in 12-hour format with AM/PM.
+ *
+ * This function takes an hour in 24-hour format and converts it to 12-hour format.
+ * It returns a string representing the hour in the format "hh AM/PM."
+ */
 export function convertTo12HourFormat(hour) {
     const hours12 = hour % 12 || 12;
     const amPm = hour < 12 ? 'AM' : 'PM';
     return `${hours12} ${amPm}`;
 }
 
+/**
+ * Converts a 12-hour format hour with AM/PM designation to a 24-hour format.
+ *
+ * @param {string} hour - The hour in 12-hour format with AM/PM (e.g., "hh:mm AM/PM").
+ * @returns {Object} An object containing the converted hours and minutes in 24-hour format.
+ *
+ * This function takes an hour in 12-hour format with AM/PM designation and converts it to 24-hour format.
+ * It returns an object with properties 'hours' and 'minutes' representing the converted values.
+ */
 export function convertTo24HourFormat(hour) {
     // Split HH:MM and AM/PM
     let [hoursMinutes, amOrPm] = hour.split(' ');
