405f90601258aa059b06e936a115f7da9765a104
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-20 10:42:10 +0800

    doc: Add documentation to buildFeelsLikeCard module

--- src/buildFeelsLikeCard.js
+++ src/buildFeelsLikeCard.js
@@ -1,11 +1,36 @@
 import buildHighlightCard from './buildHighlightCard';
 import feelsLikeImgSrc from './img/feelsLike.png';
 
+/**
+ * Builds a 'Feels Like' card using the buildHighlightCard function.
+ *
+ * @param {number} feelsLike - The 'Feels Like' temperature value to be displayed on the card.
+ * @param {string} unit - The unit of temperature measurement ('f' for Fahrenheit, 'c' for Celsius).
+ * @returns {HTMLElement} The generated 'Feels Like' card element.
+ *
+ * This function utilizes the buildHighlightCard function to create a 'Feels Like' card.
+ * The card includes a title ('Feels Like'), an icon representing temperature, the 'Feels Like' temperature value,
+ * and a description generated based on the temperature range and unit of measurement.
+ *
+ * The generated 'Feels Like' card can be appended to the DOM to visually represent the perceived temperature.
+ */
 export default function buildFeelsLikeCard(feelsLike, unit) {
     const feelsLikeCard = buildHighlightCard('Feels Like', feelsLikeImgSrc, `${feelsLike}°`, getFeelsLikeMessage(feelsLike, unit));
     return feelsLikeCard;
 }
 
+/**
+ * Generates a description based on the 'Feels Like' temperature range and unit of measurement.
+ *
+ * @param {number} feelsLike - The 'Feels Like' temperature value.
+ * @param {string} unit - The unit of temperature measurement ('f' for Fahrenheit, 'c' for Celsius).
+ * @returns {string} A descriptive message indicating the perceived weather conditions based on 'Feels Like' temperature.
+ *
+ * This function takes the 'Feels Like' temperature value and the unit of measurement as input.
+ * It categorizes the temperature into different ranges and returns a descriptive message based on the category,
+ * such as 'Hot Weather,' 'Warm Weather,' 'Mild Weather,' 'Cool Weather,' 'Cold Weather,' 'Freezing Weather,'
+ * or 'Variable Conditions.'
+ */
 function getFeelsLikeMessage(feelsLike, unit) {
     let feelsLikeMessage = '';
 
