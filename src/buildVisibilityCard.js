d9f88ab0dd5f10f80d2d786ef407e67d53623b84
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-20 10:51:42 +0800

    doc: Add documentation ot the buildVisbilityCard module

--- src/buildVisibilityCard.js
+++ src/buildVisibilityCard.js
@@ -1,13 +1,35 @@
 import buildHighlightCard from './buildHighlightCard';
 import visibilityImgSrc from './img/visibility.png';
 
+/**
+ * Builds a visibility card using the buildHighlightCard function.
+ *
+ * @param {number} visibility - The current visibility value to be displayed on the card (in kilometers).
+ * @returns {HTMLElement} The generated visibility card element.
+ *
+ * This function utilizes the buildHighlightCard function to create a visibility card.
+ * The card includes a title ('Visibility'), an icon representing visibility, the current visibility value,
+ * and a message generated based on the visibility range.
+ *
+ * The generated visibility card can be appended to the DOM to visually represent the current visibility status.
+ */
 export default function buildVisibilityCard(visibility) {
-    // Create visibility card.
+    // Create the visibility card.
     const visibilityCard = buildHighlightCard('Visibility', visibilityImgSrc, `${visibility} km`, getVisibilityMessage(visibility));
     return visibilityCard;
 }
 
-// Generate a description based on what range the current visibility falls on.
+/**
+ * Generates a description based on the range of visibility value.
+ *
+ * @param {number} visibility - The current visibility value (in kilometers).
+ * @returns {string} A descriptive message indicating the visibility conditions.
+ *
+ * This function takes the current visibility value as input and categorizes it into different ranges.
+ * It then returns a descriptive message based on the category, such as 'Excellent visibility,'
+ * 'Good visibility,' 'Moderate visibility,' 'Poor visibility,' 'Very poor visibility,' or
+ * 'Visibility information not available.'
+ */
 function getVisibilityMessage(visibility) {
     let message = '';
 
