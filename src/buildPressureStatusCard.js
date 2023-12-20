beaa6f32fb2520dbe7503813cae001d12be5ff95
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-12-20 10:45:54 +0800

    doc: Add documentation to buildPressureStatusCard module

--- src/buildPressureStatusCard.js
+++ src/buildPressureStatusCard.js
@@ -1,12 +1,35 @@
 import buildHighlightCard from './buildHighlightCard';
 import pressureImgSrc from './img/pressure.png';
 
+/**
+ * Builds a pressure status card using the buildHighlightCard function.
+ *
+ * @param {number} pressure - The current atmospheric pressure value to be displayed on the card.
+ * @returns {HTMLElement} The generated pressure status card element.
+ *
+ * This function utilizes the buildHighlightCard function to create a pressure status card.
+ * The card includes a title ('Pressure'), an icon representing pressure, the current atmospheric pressure value,
+ * and a description generated based on the pressure range.
+ *
+ * The generated pressure status card can be appended to the DOM to visually represent the current atmospheric pressure status.
+ */
 export default function buildPressureStatusCard(pressure) {
-    // Create highlight card.
+    // Create the highlight card.
     const pressureStatusCard = buildHighlightCard('Pressure', pressureImgSrc, `${pressure} hPa`, getPressureMessage(pressure));
     return pressureStatusCard;
 }
 
+/**
+ * Generates a description based on the range of atmospheric pressure value.
+ *
+ * @param {number} pressure - The current atmospheric pressure value.
+ * @returns {string} A descriptive message indicating the atmospheric pressure conditions.
+ *
+ * This function takes the current atmospheric pressure value as input and categorizes it into different ranges.
+ * It then returns a descriptive message based on the category, such as 'No pressure data available,'
+ * 'Low pressure detected,' 'Normal pressure conditions,' 'Slightly elevated pressure,'
+ * 'Moderately high pressure,' 'High pressure, stable conditions,' or 'Extremely high pressure, unusual conditions.'
+ */
 function getPressureMessage(pressure) {
     let pressureMessage;
     switch (true) {
